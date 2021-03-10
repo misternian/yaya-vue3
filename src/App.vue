b4a2d2f04da61a48cb129a4ad02ff3938fc6c437
Nian <talk2nian@gmail.com>
2021-03-10 16:27:04 +0800

    feat: 一点小改动

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <template>
-  <div class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+    <Header />
     <router-view />
-    <Footer />
-  </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,6 @@
 <template>
   <div>
-    <p>
-      <a href="https://github.com/antfu/vitesse" target="_blank"> Vitesse </a>
-    </p>
-    <p class="text-sm opacity-50">
-      <em>intro</em>
-    </p>
-
-    <div class="py-4" />
-
-    <input
-      v-model="name"
-      placeholder="What's your name"
-      class="px-4 py-2 border border-gray-200 rounded text-center text-sm outline-none active:outline-none bg-transparent dark:border-gray-700"
-      style="width: 250px"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button class="btn m-3 text-sm" :disabled="!name" @click="go">go</button>
-    </div>
+    hello everyone
   </div>
 </template>
 
